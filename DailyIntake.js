e3efc4fb50e6a74f52398b3a35879e4022fdb9a4
thedylemma <92330171+thedylemma@users.noreply.github.com>
2023-03-15 14:45:08 +0000

    Started new UI and finished daily intake class
    DailyIntake class is now finished and I have started work on a new UI for the user to navigate, pretty much scrapping the original UI which was bare bones anyway.

--- DailyIntake.js
+++ DailyIntake.js
@@ -1,21 +1,56 @@
 class DailyIntake {
     constructor() {
         this.meals = [];
+        this.date = new Date().toLocaleDateString('en-US');
+    }
+
+    addMeals(meals) {
+        this.meals = this.meals.concat(meals);
+    }
+
+    addMeal(meal) {
+        this.meals.push(meal);
+    }
+
+    removeMeal(index) {
+        this.meals.pop(index, 1);
     }
 
     getMeals() {
         return this.meals;
     }
 
+    //very similar method to Meal's calculating ingredients.
+    //combine all macros of all meals
     calculateMacros() {
         let macros = [];
-        //loop through every meal, and calculate the macros.
-        for (var i = 0; i < this.meals.length; i++) {
-            //loop through the meal's macros.
-            this.meals[i].getMacros().forEach(macro => {
-                //check if macro is present in macros array, if not append and incriment.
-                
-            });
+        this.meals.forEach(meal => {
+           let mealMacros = meal.calculateMacros();
+           mealMacros.forEach(macro => { 
+                //check if the name is present.
+                let existingMacroIndex = this.getMacroIndex(macro['name'], macros);
+                if (existingMacroIndex > -1){
+                    //incriment existing element.
+                    macros[existingMacroIndex]['value'] = parseFloat(macros[existingMacroIndex]['value']) + parseFloat(macro['value']);
+                } else {
+                    //create and incriment new element.
+                    macros.push({'name':macro['name'], 'value':macro['value'], 'unit':macro['unit']});
+                }
+           })
+        });
+        return(macros);
+    }
+
+    getMacroIndex(macroName, macroArray) {
+        for (var i = 0; i < macroArray.length; i++) {
+            if (macroArray[i]['name'] == macroName) {
+                return i;
+            }
         }
+        return -1;
+    }
+
+    getDate() {
+        return this.date;
     }
 }
